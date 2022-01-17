593c6fd7f3e3e1c85aa380ca7e6ee24a6776a6f8
Wyatt C <wcastellan@yahoo.com>
2022-01-17 12:06:47 -0500

    add javascript folder to public folder

--- models/index.js
+++ models/index.js
@@ -11,3 +11,29 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: 'SET NULL'
 });
+
+User.belongsToMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.belongsToMany(User, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
