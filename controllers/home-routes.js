261b165076b6a9eaf677b817e06eeb6f7c02b275
Wyatt C <wcastellan@yahoo.com>
2022-01-19 13:13:19 -0500

    make changes to controllers and api folder files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -86,4 +86,43 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/posts-comments', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'content', 'title', 'created_at'],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                modle: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attribues: ['username']
+        }
+    ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found wit hthis id' });
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+
+        res.render('posts-comments', { post, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
